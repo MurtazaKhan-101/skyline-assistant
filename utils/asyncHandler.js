4673de531ac8c9f856dabae7e8b14dad0f79780a
MurtazaKhan-101 <murtazakhan1617@gmail.com>
2025-08-21 18:20:31 +0500

    Code Red

--- utils/asyncHandler.js
+++
@@ -1,6 +0,0 @@
-// Helper function to handle async errors
-const asyncHandler = (fn) => (req, res, next) => {
-  Promise.resolve(fn(req, res, next)).catch(next);
-};
-
-module.exports = asyncHandler;
